140203933dd37c28112e070d4680afbca9aa6b89
kosinv00 <pixel.l.player@gmail.com>
2022-12-07 11:57:39 +0100

    Day05.03

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,13 +1,29 @@
+import java.awt.CardLayout
+import java.text.BreakIterator
+
 class Day05:puzzle<Int>("Day05",0,0) {
-    override fun part1(input: Input): Int {
-        val  Stacks: Stack = input.
+    override fun part1(input: Input): Int    {
+        var stacks = createStacks(input)
+        print(stacks)
         return 0
     }
+
+    fun createStacks(input: List<String>): List<MutableList<Char>>{
+        val stacks = mutableListOf<MutableList<Char>>()
+        for(i in 0 .. 20){ stacks.add(mutableListOf())}
+        var count= 0
+        while (input[count+1].isNotBlank()){
+            for (i in input[count].indices) {
+                if ((i+3) % 4  == 0 && input[count][i].toString() != " "){ stacks[(i+3)/4].add(input[count][i])}
+            }
+            count +=1
+        }
+        return stacks.filter { it.isNotEmpty()}
+    }
+
 
     override fun part2(input: Input): Int {
         return 0
     }
 
-    class Stack(val nr: Int, createList : List<String>) {
-    }
 }
